3357364cc9ee226a89f222b4c5dfb6759019e00d
Erik Ruhmanis <erik2310@edu.easj.dk>
2017-12-14 20:19:11 +0100

    Initial commit

---
+++ src/Variabler.java
@@ -0,0 +1,12 @@
+public class Variabler {
+
+    // Erklæring og initialisering af en instans variabel
+    int instanceVariable = 0;
+
+    public static void main(String[] args) {
+
+        // Erklæring og initialisering af en lokal variabel
+        int localVariable = 0;
+
+    }
+}
