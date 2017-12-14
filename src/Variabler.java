942671bcd78ea838d52cbd099a74cac2d7e03a5a
Erik Ruhmanis <erik2310@edu.easj.dk>
2017-12-14 22:14:59 +0100

    Typecasting fra double til int

--- src/Variabler.java
+++ src/Variabler.java
@@ -8,5 +8,13 @@ public class Variabler {
         // Erklæring og initialisering af en lokal variabel
         int localVariable = 0;
 
+        // Et double tal værdi
+        double doubleTal = 22.5;
+
+        // Bliver typecastet til int og gemt i intTal
+        int intTal = (int)doubleTal;
+
+        // Udskriver intTal værdien
+        System.out.println(intTal);
     }
 }
